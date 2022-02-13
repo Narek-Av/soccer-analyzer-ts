a5fa0f863d12a5822620d2dc9a87c534cf623331
Av-Narek <nar.avetisyan93@gmail.com>
2022-02-13 23:50:29 +0400

    Added user name from new router

---
+++ src/components/User/User.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+import './User.css';
+
+const User: FunctionComponent = () => {
+  return (
+    <div>User Info</div>
+  )
+}
+
+export default User;
